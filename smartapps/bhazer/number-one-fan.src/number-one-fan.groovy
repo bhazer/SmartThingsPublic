3d91739e8d2ac65fddc399b47224bb65caad90cd
Bart Hazer <bartley@yahoo-inc.com>
2016-11-08 13:10:05 -0800

    tabs to spaces

--- smartapps/bhazer/number-one-fan.src/number-one-fan.groovy
+++ smartapps/bhazer/number-one-fan.src/number-one-fan.groovy
@@ -1,35 +1,35 @@
 definition(
-    name: "Number One Fan",
-    namespace: "bhazer",
-    author: "Bart Hazer",
-    description: "Fan control",
-    category: "Convenience",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+        name: "Number One Fan",
+        namespace: "bhazer",
+        author: "Bart Hazer",
+        description: "Fan control",
+        category: "Convenience",
+        iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+        iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+        iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 
 preferences {
-	section("Switch") {
-		input "theswitch", "capability.switch", required: true
-	}
-	section("Times") {
-		input "time1", "number", title: "How long to stay on when switched on?", required: true
-		input "time2", "number", title: "How long to stay on when double tapped?", required: true
-	}
+    section("Switch") {
+        input "theswitch", "capability.switch", required: true
+    }
+    section("Times") {
+        input "time1", "number", title: "How long to stay on when switched on?", required: true
+        input "time2", "number", title: "How long to stay on when double tapped?", required: true
+    }
 }
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"
+    log.debug "Installed with settings: ${settings}"
 
-	initialize()
+    initialize()
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
+    log.debug "Updated with settings: ${settings}"
 
-	unsubscribe()
-	initialize()
+    unsubscribe()
+    initialize()
 }
 
 def initialize() {
@@ -37,8 +37,8 @@ def initialize() {
     state.offAt = -1
 
     subscribe(theswitch, "switch.on", switchedOn)
-	subscribe(theswitch, "switch.off", switchedOff)
-	subscribe(theswitch, "button.pushed", pushed)
+    subscribe(theswitch, "switch.off", switchedOff)
+    subscribe(theswitch, "button.pushed", pushed)
 }
 
 def switchedOn(evt) {
